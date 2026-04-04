a4ec4d593b1e629d36c3418ab75f9489fe29ac01
nils-nyberg <nilsnyberg24@hotmail.com>
2026-04-04 17:33:02 +0200

    Add header styling for the Layout component

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import logo from "./assets/logo.png";
 
 function Layout() {
   return (
     <>
-      <h1>Hello Layout!</h1>
+      <section>
+        <div className="header">
+          <img className="header__logo" src={logo} alt="Logo" />
+          <h1 className="header__title">The Blog</h1>
+          {/* The language icon styling is temporary for future development. */}
+          <span className="header__language-icon"></span>
+        </div>
+      </section>
       <main>
         <Outlet />
       </main>
