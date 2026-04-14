47c682eb4f1227d5a3eaa5fb134f9eef035a3dd4
nils-nyberg <nilsnyberg24@hotmail.com>
2026-04-14 20:16:37 +0200

    Change section element to header

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -4,14 +4,14 @@ import logo from "./assets/logo.png";
 function Layout() {
   return (
     <>
-      <section>
+      <header>
         <div className="header">
           <img className="header__logo" src={logo} alt="Logo" />
           <h1 className="header__title">The Blog</h1>
           {/* The language icon styling is temporary for future development. */}
           <span className="header__language-icon"></span>
         </div>
-      </section>
+      </header>
       <main>
         <Outlet />
       </main>
