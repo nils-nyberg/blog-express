ebf9fd7ae13cc2223fcac415ffde37409190f647
nils-nyberg <nilsnyberg24@hotmail.com>
2026-05-08 14:02:07 +0200

    Clean up language change logic + conditional rendering for website title

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,20 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import logo from "./assets/logo.png";
 
 function Layout() {
   const { t, i18n } = useTranslation();
+  const { pathname } = useLocation();
+
+  const isPageAuth = pathname === "/auth";
+
+  const handleLanguageChange = () => {
+    i18n.changeLanguage(t("header.changeLanguage").toLowerCase());
+  };
+
   return (
     <>
       <header>
         <div className="header">
           <img className="header__logo" src={logo} alt="Logo" />
-          <h1 className="header__title">The Blog</h1>
+          {isPageAuth && <h1 className="header__title">The Blog</h1>}
           <button
             className="header__language-button"
-            onClick={() => {
-              i18n.changeLanguage(t("header.changeLanguage").toLowerCase());
-            }}
+            onClick={handleLanguageChange}
           >
             {t("header.changeLanguage")}
           </button>
