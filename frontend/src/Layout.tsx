02f51cf7466b07823aaf2b08f4f47ba32365335c
nils-nyberg <nilsnyberg24@hotmail.com>
2026-05-10 16:44:48 +0200

    Fully implement navigation functionality

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,7 +1,12 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import logo from "./assets/logo.png";
 import useWindowDimensions from "./utils/hooks/useWindowDimensions";
+
+type Props = {
+  href: string;
+  children: string;
+};
 
 function Layout() {
   const { t, i18n } = useTranslation();
@@ -14,6 +19,14 @@ function Layout() {
     i18n.changeLanguage(t("header.changeLanguage").toLowerCase());
   };
 
+  function CustomLink({ href, children }: Props) {
+    return (
+      <Link to={href} className={pathname === href ? "active" : ""}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <>
       <header>
@@ -22,11 +35,17 @@ function Layout() {
           {(width <= 1400 || isPageAuth) && (
             <h1 className="header__title">The Blog</h1>
           )}
-          <div className="header__navbar">
-            <a className="active">{t("header.navigation.blog")}</a>
-            <a>{t("header.navigation.friends")}</a>
-            <a>{t("header.navigation.profile")}</a>
-          </div>
+          {!isPageAuth && (
+            <div className="header__navbar">
+              <CustomLink href="/">{t("header.navigation.blog")}</CustomLink>
+              <CustomLink href="/friends">
+                {t("header.navigation.friends")}
+              </CustomLink>
+              <CustomLink href="/profile">
+                {t("header.navigation.profile")}
+              </CustomLink>
+            </div>
+          )}
           <button
             className="header__language-button"
             onClick={handleLanguageChange}
